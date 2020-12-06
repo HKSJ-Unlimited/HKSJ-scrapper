0a2dec63be121b0349a454f80c90a92f76a34ac3
rohit <rocknegi53@gmail.com>
2020-12-06 23:07:34 +0530

    create scrapper for searching files in main app

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const axios = require('axios').default;
+const fs = require('fs');
+
+const BASE_URL = process.env.BASE_URL;
+let currentStr = '';
+
+const fetchData = async (name = '') => {
+    try {
+        return await axios.post(`${BASE_URL}${name}`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const writeToFile = () => {
+    fs.appendFileSync('scrapped.txt', currentStr, (err) => {
+        if (err)
+            console.log(err);
+    })
+}
+
+const saveCateogoryData = async (name) => {
+    if (!name)
+        return
+    const categoryData = await fetchData(`${name}/`);
+    if (categoryData.data.files) {
+        categoryData.data.files.map(item => {
+            currentStr = `${name}/${item.name}$`
+            writeToFile();
+        });
+    }
+
+}
+
+const startScrapping = async () => {
+    const categories = await fetchData();
+    if (categories.data) {
+        const categoryList = categories.data.files;
+        categoryList.forEach(item => {
+            if (item.name !== 'snapchats' || item.name !== 'Celebrity')
+                saveCateogoryData(item.name);
+        })
+    }
+}
+
+Promise.all([
+    startScrapping()
+])
+    .then(value = () => {
+        // writeToFile()
+    });
